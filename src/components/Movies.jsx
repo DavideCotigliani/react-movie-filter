433c5ccb49d77bb613d0d9458e8a8c6ddca1763b
DavideCotigliani <davide.cotigliani@gmail.com>
2025-05-12 14:54:27 +0200

    [UPD] Filtered the array with map

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,7 +1,19 @@
 
 const Movies = () => {
+    const moviesArray = [
+        { title: 'Inception', genre: 'Fantascienza' },
+        { title: 'Il Padrino', genre: 'Thriller' },
+        { title: 'Titanic', genre: 'Romantico' },
+        { title: 'Batman', genre: 'Azione' },
+        { title: 'Interstellar', genre: 'Fantascienza' },
+        { title: 'Pulp Fiction', genre: 'Thriller' },
+    ]
     return (
-        <div></div>
+        <ul>
+            {moviesArray.map((movie) => (
+                <li key={movie.title}> {movie.title} - {movie.genre}</li>
+            ))}
+        </ul>
     )
 }
 
